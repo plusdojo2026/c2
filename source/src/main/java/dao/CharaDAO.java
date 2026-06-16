479ef330a10f14df5bc46b0da213ad855784e104
plusdojo2026-shiraishi <shiraishi-daito-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-16 13:11:40 +0900

    C2

---
+++ source/src/main/java/dao/CharaDAO.java
@@ -0,0 +1,68 @@
+package dao;
+
+import java.sql.Connection;
+
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class CharaDAO {
+	public Chara findByUserId(int charaId) {
+	Connection conn = null;
+	Chara chara = null;
+	
+	try {
+
+		// JDBCドライバを読み込む
+		Class.forName("com.mysql.cj.jdbc.Driver");
+
+		// データベースに接続する
+		conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/webapp1?"
+			+ "characterEncoding=utf8&useSSL=false&serverTimezone=GMT%2B9&rewriteBatchedStatements=true",
+				"root", "daito110");
+		
+		// SELECT文を準備する
+		String sql = "SELECT * FROM chara WHERE chara_id = ?";
+		PreparedStatement pStmt = conn.prepareStatement(sql);
+		
+		//SQl文を完成させる
+		pStmt.setInt(1,charaId);
+		
+		//SELECT文を実行し、結果表（ResultSet）を取得する
+		ResultSet rs = pStmt.executeQuery();
+		if(rs.next()) {
+
+            chara = new Chara(
+
+                rs.getInt("chara_id"),
+
+                rs.getString("image"),
+
+                rs.getString("speak"),
+
+                rs.getInt("type_id")
+            	);
+        }
+
+    } catch(Exception e) {
+
+        e.printStackTrace();
+
+    } 
+		
+	} finally {
+		// データベースを切断
+		if (conn != null) {
+		try {
+				conn.close();
+			} catch(SQLException e) {
+		e.printStackTrace();
+			}
+		}
+	}
+	//結果を返す
+	return chara;
+	
+}
+
