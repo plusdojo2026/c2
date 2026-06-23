2b243554159f90fbb91d3abe8741c12dba389290
plusdojo2026-satoarisa <sato-arisa-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-23 16:14:35 +0900

    C2

--- source/src/main/java/servlet/NameServlet.java
+++ source/src/main/java/servlet/NameServlet.java
@@ -13,89 +13,84 @@ import javax.servlet.http.HttpSession;
 import dao.NameDAO;
 import dto.UserDto;
 
-
 /**
-	* Servlet implementation class NameServlet
-*/
+ * Servlet implementation class NameServlet
+ */
 @WebServlet("/NameServlet")
 
-public class NameServlet extends HttpServlet{
+public class NameServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
+
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
 	 *      response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-	
-		throws ServletException, IOException {
+
+			throws ServletException, IOException {
 		// もしもログインしていなかったらログインサーブレットにリダイレクトする
 		HttpSession session = request.getSession();
 		if (session.getAttribute("loginId") == null) {
-			response.sendRedirect(request.getContextPath() +"/LoginServlet");
+			response.sendRedirect(request.getContextPath() + "/LoginServlet");
 			return;
 		}
-		
-		//ログイン中のユーザIDを取得
-		Integer userId = (Integer) session.getAttribute("userId");	
 
-		//DBから現在の呼び名を取得する
-		NameDAO nDao=new NameDAO();
-		UserDto user=nDao.selectByUserId(userId);
-		
+		// ログイン中のユーザIDを取得
+		Integer userId = (Integer) session.getAttribute("userId");
+
+		// DBから現在の呼び名を取得する
+		NameDAO nDao = new NameDAO();
+		UserDto user = nDao.selectByUserId(userId);
+
 		request.setAttribute("user", user);
-		
+
 		// キャラとユーザの呼び方を登録できるページにフォワードする
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/name.jsp");
 		dispatcher.forward(request, response);
 	}
-	
+
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
 	 *      response)
 	 */
-	
+
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		// もしもログインしていなかったらログインサーブレットにリダイレクトする
 		HttpSession session = request.getSession();
 		if (session.getAttribute("loginId") == null) {
-			response.sendRedirect(request.getContextPath() +"/LoginServlet");
+			response.sendRedirect(request.getContextPath() + "/LoginServlet");
 			return;
 		}
-		
+
 		// リクエストパラメータを取得する
 		request.setCharacterEncoding("UTF-8");
-		//ログイン中のユーザIDを取得する。これでユーザごとにニックネームが決められると思い、記載した。
-		int userId=(Integer)session.getAttribute("userId");	
-		String userNickname=request.getParameter("userNickname");
-		String charaNickname=request.getParameter("charaNickname");
-		
-		
-		
+		// ログイン中のユーザIDを取得する。これでユーザごとにニックネームが決められると思い、記載した。
+		int userId = (Integer) session.getAttribute("userId");
+		String userNickname = request.getParameter("userNickname");
+		String charaNickname = request.getParameter("charaNickname");
+
 		// 更新処理を行う
 		NameDAO nDao = new NameDAO();
 		boolean isSuccess = nDao.update(new UserDto(userId, userNickname, charaNickname));
-		
-		 
-		String message;
-		
-		if(isSuccess) {
-			message="呼び名を更新しました。";
-		}else {
-			message="呼び名を更新できませんでした。";
+
+		if (isSuccess) {
+
+			response.sendRedirect(request.getContextPath() + "/HomeServlet");
+
+			return;
 		}
-		
-		request.setAttribute("message",message);
-		
-		 // 更新後のデータを再取得する
-	    UserDto user = nDao.selectByUserId(userId);
-	    request.setAttribute("user", user);
-		
-		// 結果ページにフォワードする
+
+		// 失敗時だけname.jspへ戻す
+		request.setAttribute("message", "呼び名を更新できませんでした。");
+
+		UserDto user = nDao.selectByUserId(userId);
+
+		request.setAttribute("user", user);
+
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/name.jsp");
+
 		dispatcher.forward(request, response);
 	}
 }
-
-
