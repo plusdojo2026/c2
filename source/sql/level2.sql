de1898d962c0ea05c67103b0fa90adcfeae9ca01
plusdojo2026-yokoyamadaiki <yokoyama-daiki-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-24 15:39:45 +0900

    c2

---
+++ source/sql/level2.sql
@@ -0,0 +1,88 @@
+INSERT INTO daily_mission (user_id,mission_id,daily,complete) VALUES(
+    2,6,'2026-06-12',true
+);
+
+INSERT INTO daily_mission (user_id,mission_id,daily,complete) VALUES(
+    2,10,'2026-06-12',false
+);
+
+INSERT INTO daily_mission (user_id,mission_id,daily,complete) VALUES(
+    2,7,'2026-06-12',true
+);
+
+INSERT INTO daily_mission (user_id,mission_id,daily,complete) VALUES(
+    2,11,'2026-06-13',true
+);
+
+INSERT INTO daily_mission (user_id,mission_id,daily,complete) VALUES(
+    2,12,'2026-06-13',true
+);
+
+INSERT INTO daily_mission (user_id,mission_id,daily,complete) VALUES(
+    2,7,'2026-06-13',true
+);
+
+INSERT INTO daily_mission (user_id,mission_id,daily,complete) VALUES(
+    2,15,'2026-06-14',true
+);
+
+INSERT INTO daily_mission (user_id,mission_id,daily,complete) VALUES(
+    2,18,'2026-06-14',true
+);
+
+INSERT INTO daily_mission (user_id,mission_id,daily,complete) VALUES(
+    2,21,'2026-06-14',true
+);
+
+
+INSERT INTO daily_mission (user_id,mission_id,daily,complete) VALUES(
+    2,22,'2026-06-15',true
+);
+
+INSERT INTO daily_mission (user_id,mission_id,daily,complete) VALUES(
+    2,23,'2026-06-15',true
+);
+
+INSERT INTO daily_mission (user_id,mission_id,daily,complete) VALUES(
+    2,24,'2026-06-15',true
+);
+
+INSERT INTO daily_mission (user_id,mission_id,daily,complete) VALUES(
+    2,31,'2026-06-16',true
+);
+
+INSERT INTO daily_mission (user_id,mission_id,daily,complete) VALUES(
+    2,32,'2026-06-16',true
+);
+
+INSERT INTO daily_mission (user_id,mission_id,daily,complete) VALUES(
+    2,36,'2026-06-16',true
+);
+
+INSERT INTO daily_mission (user_id,mission_id,daily,complete) VALUES(
+    2,37,'2026-06-17',true
+);
+
+INSERT INTO daily_mission (user_id,mission_id,daily,complete) VALUES(
+    2,38,'2026-06-17',true
+);
+
+INSERT INTO daily_mission (user_id,mission_id,daily,complete) VALUES(
+    2,38,'2026-06-17',false
+);
+
+INSERT INTO daily_mission (user_id,mission_id,daily,complete) VALUES(
+    2,45,'2026-06-18',true
+);
+
+INSERT INTO daily_mission (user_id,mission_id,daily,complete) VALUES(
+    2,44,'2026-06-18',true
+);
+
+INSERT INTO daily_mission (user_id,mission_id,daily,complete) VALUES(
+    2,43,'2026-06-18',true
+);
+
+
+
+SELECT * FROM daily_mission;
